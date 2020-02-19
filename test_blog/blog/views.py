5de8bc1a7342142e57dd85e3323b1ba5b2f31231
Sergei Alekseev <salvicode@gmail.com>
2020-02-19 12:44:00 +0600

    - Templates for displaying posts

--- test_blog/blog/views.py
+++ test_blog/blog/views.py
@@ -1,3 +1,15 @@
+from django.views import generic
+from .models import Post
 from django.shortcuts import render
 
-# Create your views here.
+class PostLists(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+def index(request):
+    my_dict = {"insert_me": "I am from views.py"}
+    return render(request,'index.html',context=my_dict)
