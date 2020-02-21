bda468fc0e10401254a8ee5e7fda3314c0dd8b8f
Sergei Alekseev <salvicode@gmail.com>
2020-02-21 16:30:12 +0600

    - Added PostgreSQL settings

--- test_blog/test_blog/settings.py
+++ test_blog/test_blog/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'test_blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_blog_example',
+        'USER': 'test_user',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
