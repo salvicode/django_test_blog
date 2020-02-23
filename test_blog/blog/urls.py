4883f32841694d6fd4068fe65a3931b2ed2bf556
Sergei Alekseev <salvicode@gmail.com>
2020-02-23 15:14:03 +0600

    - Feeds integration

--- test_blog/blog/urls.py
+++ test_blog/blog/urls.py
@@ -1,9 +1,11 @@
 from . import views
 from django.urls import path
+from .feeds import LatestPostsFeed
 
 urlpatterns = [
     #path('', views.PostLists.as_view(), name='home'), 
     path('', views.PostLists, name='home'),   
     #path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path("<slug:slug>/", views.post_detail, name="post_detail"),
+    path("feed/rss", LatestPostsFeed(), name="post_feed"),
 ]
